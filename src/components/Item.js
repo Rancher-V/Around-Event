6371ba4f0efad175182b86575ff9ce15322e92aa
Rancher-V <yangmingxiework@gmail.com>
2017-12-31 01:24:04 -0500

    set favorite on page, but not interactive with server

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import $ from 'jquery'
+import {API_ROOT} from '../constants'
 
 export class Item extends React.Component{
+    state={
+        isFavorite: false,
+    }
+
+    componentDidMount(){
+        this.setState({
+            isFavorite: this.props.item.favorite,
+        })
+    }
+
+    setFavorite=()=>{
+        console.log(this.state.isFavorite)
+        return $.ajax({
+            url: `${API_ROOT}/history`,
+            method: this.state.isFavorite ? 'DELETE' : 'POST',
+            crossDomain: true,
+            //headers -> authorization
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json',
+            data: {
+                'user_id': 1111,
+                'favorite': this.props.item.item_id
+            },
+        }).then((response)=>{
+            this.setState((prevState)=>({isFavorite: !prevState.isFavorite}))
+            console.log(response);
+        }, (error)=>{
+            this.setState({error: error.responseText});
+        }).then(()=>{
+            this.setState({loadingActivities: false});
+        }).catch((error)=>{
+            console.log(error)
+        });
+    }
+
+
+
+
+
     render(){
-        const { image_url, categories, name, url, favorite, address, city, state, item_id} = this.props.item;
+        const { image_url, categories, name, url, address, city, state, item_id} = this.props.item;
         return(
             <li className="item" id={item_id}>
                 <img alt="item" src={image_url} />
@@ -12,7 +53,7 @@ export class Item extends React.Component{
                 </div>
                 <p className="item-address">{address}<br/>{city}<br/> {state}</p>
                 <div className="fav-link">
-                    <i className={favorite ? "fa fa-heart" : "fa fa-heart-o"}></i>
+                    <i className={this.state.isFavorite ? "fa fa-heart" : "fa fa-heart-o"} onClick={this.setFavorite}></i>
                 </div>
             </li>
         )
--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,5 @@ export const GEO_OPTION={
     maximumAge: 3600000,
     timeout: 27000,
 };
-
 export const POS_KEY='POS_KEY';
-
 export const API_ROOT= 'http://localhost:8080/Titan';
