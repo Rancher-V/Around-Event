5427b379d1dd7b1b63013450ea53bd6c8085b35b
unknown <yangmingxiework@gmail.com>
2018-01-08 22:42:39 -0500

    could not find way to transfer event from leftTab to loading activities

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,7 @@ import {LeftTabs} from './LeftTabs'
 import {Itemlist} from './Itemlist'
 import {Spin} from 'antd'
 import {TopBanner} from './TopBanner'
-import {API_ROOT, GEO_OPTION, POS_KEY} from '../constants'
-import $ from 'jquery'
+import {GEO_OPTION, POS_KEY} from '../constants'
 
 export class Home extends  React.Component{
     state={
@@ -16,34 +15,10 @@ export class Home extends  React.Component{
         loadingRecommend: false,
         loadingGeo: false,
         error: '',
-        activities: [],
     }
 
     componentDidMount(){
         this.getGeoLocation();
-        if(this.state.error === ''){
-            const url = this.settingUrlwithLodingState();
-            this.loadingEvents(url);
-        }
-    }
-
-    componentDidUpdate(){
-        console.log(this.state.selectedTab);
-
-    }
-
-    settingUrlwithLodingState=(selectedTab, location, radius)=>{
-        selectedTab = selectedTab ? selectedTab : 'Nearby'
-        const {lat, lon} = location ? location : JSON.parse(localStorage.getItem("POS_KEY"));
-        const range = radius? radius : 20;
-        //this.setState({loadingActivities: true, loading: true, loadingDescription: 'Loading Interesting Events Around You Now...'});      //callback
-
-        switch (selectedTab) {
-            case "Nearby":   return `${API_ROOT}/search?user_id=1111&lat=${lat}&lon=${lon}&radius=${range}`;
-            case "My Favorites": return `${API_ROOT}/history?user_id=1111`;
-            case "Recommendation":  return `${API_ROOT}/recommendation?user_id=1111&lat=${lat}&lon=${lon}&radius=${range}`;
-            default:      return ``;
-        }
     }
 
     onSuccessLoadGeoLocation=(position)=>{
@@ -51,6 +26,7 @@ export class Home extends  React.Component{
         const {latitude: lat, longitude: lon} = position.coords;
         console.log(position.coords);
         localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, lon: lon}));
+        //this.loadingEvents();
         //this.loadingNearbyActivities();
     }
 
@@ -74,35 +50,14 @@ export class Home extends  React.Component{
         }
     }
 
+
+
+
     onSelectedTab=(tabName)=>{
         this.setState({
             selectedTab: tabName,
         });
-
-        if(this.state.error === ''){
-            const url = this.settingUrlwithLodingState(tabName);
-            console.log(url)
-            this.loadingEvents(url);
-        }
     }
-
-    loadingEvents=(url)=>{
-        return $.ajax({
-            url: url,
-            method: 'GET',
-            //headers -> authorization
-        }).then((response)=>{
-            this.setState({activities: response, error:''});
-            console.log(this.state.activities);
-        }, (error)=>{
-            this.setState({error: error.responseText});
-        }).then(()=>{
-            this.setState({loadingActivities: false, loading: false, loadingDescription: ''});
-        }).catch((error)=>{
-            console.log(error)
-        });
-    }
-
 
     render(){
         const loadingArea=(
@@ -110,7 +65,8 @@ export class Home extends  React.Component{
                 <LeftTabs onSelectedTab={this.onSelectedTab}
                           selectedTab={this.state.selectedTab}
                 />
-                <Itemlist   activities={this.state.activities}
+                <Itemlist   selectedTab={this.state.selectedTab}
+                            loadingErr = {this.state.error}
                 />
             </section>
         )
--- src/components/Itemlist.js
+++ src/components/Itemlist.js
@@ -1,19 +1,68 @@
 import React from 'react'
 import {Item} from './Item'
+import {API_ROOT} from '../constants'
+import $ from 'jquery'
 
 
 export class Itemlist extends React.Component{
 
+    state={
+        activities: [],
+        loading: false,
+        loadingDescription: '',
+        loadingActivities: false,
+    }
+    componentDidMount(){
+        if(this.props.loadingErr === '')
+            this.loadingEvents();
+    }
+
+    componentDidUpdate(){
+        console.log(this.props.selectedTab);
+    }
+
+    settingUrlwithLodingState=(selectedTab, location, radius)=>{
+        selectedTab = selectedTab ? selectedTab : 'Nearby'
+        const {lat, lon} = location ? location : JSON.parse(localStorage.getItem("POS_KEY"));
+        const range = radius? radius : 20;
+        //this.setState({loadingActivities: true, loading: true, loadingDescription: 'Loading Interesting Events Around You Now...'});      //callback
+
+        switch (selectedTab) {
+            case "Nearby":   return `${API_ROOT}/search?user_id=1111&lat=${lat}&lon=${lon}&radius=${range}`;
+            case "My Favorites": return `${API_ROOT}/history?user_id=1111`;
+            case "Recommendation":  return `${API_ROOT}/recommendation?user_id=1111&lat=${lat}&lon=${lon}&radius=${range}`;
+            default:      return ``;
+        }
+    }
+
+    loadingEvents=(location, radius)=>{
+        return $.ajax({
+            url: this.settingUrlwithLodingState(this.props.selectedTab, location, radius),
+            method: 'GET',
+            //headers -> authorization
+        }).then((response)=>{
+            this.setState({activities: response, error:''});
+            console.log(this.state.activities);
+        }, (error)=>{
+            this.setState({error: error.responseText});
+        }).then(()=>{
+            this.setState({loadingActivities: false, loading: false, loadingDescription: ''});
+        }).catch((error)=>{
+            console.log(error)
+        });
+    }
+
     render(){
+        // if(this.props.loadingErr === '')
+        //     this.loadingEvents()
         return(
             <ul className="item-list">
-                {this.props.activities? this.props.activities.map((item)=>
+                {this.state.activities? this.state.activities.map((item)=>
                     <Item
                         key={item.item_id}
                         item={item}
                     />) : null }
             </ul>
-
         )
     }
 }
